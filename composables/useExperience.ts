5d62b881cb30bdceaa7bc8a60911bd0732f158bd
ِAbdelrahman Mahmoud <abdelrahmannali14@gmail.com>
2025-09-28 16:57:06 +0300

    minor edits in experience

--- composables/useExperience.ts
+++ composables/useExperience.ts
@@ -4,14 +4,17 @@ export function useExperience(){
         date: '08/2025 – Present',
           role: 'Full Stack Engineer',
           company: 'Homains',
-          desc: [],
+          desc: ["Translated business requirements into scalable technical solutions, which enabled 3+ key initiatives and reduced project delivery time by 30%.", "Partnered with cross-functional stakeholders to translate business needs into technical solutions, delivering 10+ successful product features that aligned with strategic goals. "],
           logo: "/homains-logo.png"
       },  
       {
-          date: '03/2025 – 08/2025',
+          date: '12/2024 – 08/2025',
           role: 'Frontend Developer',
           company: 'Homains',
-          desc: ["Delivered responsive web application using Vue, Nuxt, TypeScript, and Vuetify based on Figma designs.", "Took ownership of reviewing and refactoring legacy code to better match design specifications and user flows, resulting in a smoother and more maintainable UI.","Collaborated with backend developers to debug integration issues, adjust APIs, and ensure full end-to-end functionality."],
+          desc: ["Delivered responsive web apps using Angular, Vue, Nuxt, TypeScript, Vuetify, and NG Zorro.",  
+            "Reviewed and optimized misaligned code, resolving 50+ UI inconsistencies and improving feature usability scores by 15%. Integrated APIs and ensured performance optimization by increasing performance by 70%. ",
+            "Transformed 20+ Figma designs into pixel-perfect, responsive Portal & Dashboard UIs.",
+            "Actively contributed to Agile workflows and cross-functional collaboration using Git & Git-Flow."],
           logo: "/homains-logo.png"
         },
         
@@ -19,21 +22,21 @@ export function useExperience(){
           date: '04/2025 – 06/2025',
           role: 'Frontend Developer',
           company: 'Start for Business Development',
-          desc: ["Developed responsive e-commerce interfaces from Figma designs using HTML, SCSS, Bootstrap and integrated them within Django Views.","Leveraged AI tools like Cursor to boost development productivity and learned to craft effective prompts to generate high-quality, production-ready code."],
+          desc: ["• Converted 25+ Figma designs into pixel-perfect, responsive E-Commerce UIs using HTML, SCSS, Bootstrap, improving design-to-development turnaround by 50% and boosting client satisfaction."],
           logo: "/start-for-business-dev.jpeg"
         },
-        {
-          date: '12/2024 – 03/2025',
-          role: 'Intern Frontend Developer',
-          company: 'Homains',
-          desc: ["Led the development of responsive UIs from Figma designs using Angular, and Ant-design.", "Independently integrated backend APIs via Angular Services, ensuring performance optimization (LCP, CLS).", "Actively contributed to Agile workflows and cross-functional collaboration using Git & Git-Flow.", "Recognized as a top performer in the internship program.", "Took ownership of the Homains internship web app, leading development of core pages and delivering the largest feature set."],
-          logo: "/homains-logo.png"
-        },
+        // {
+        //   date: '12/2024 – 03/2025',
+        //   role: 'Intern Frontend Developer',
+        //   company: 'Homains',
+        //   desc: ["Led the development of responsive UIs from Figma designs using Angular, and Ant-design.", "Independently integrated backend APIs via Angular Services, ensuring performance optimization (LCP, CLS).", "Actively contributed to Agile workflows and cross-functional collaboration using Git & Git-Flow.", "Recognized as a top performer in the internship program.", "Took ownership of the Homains internship web app, leading development of core pages and delivering the largest feature set."],
+        //   logo: "/homains-logo.png"
+        // },
         {
           date: '10/2023 – 01/2024',
           role: 'Frontend Developer',
           company: 'EPIK for Software Solutions',
-          desc: ['Transformed Figma Designs to pixel-perfect interfaces.'],
+          desc: [' Implemented 10+ Figma designs into pixel-perfect and responsive UIs.'],
           logo: "/epik.jpeg"
         },
       ]
