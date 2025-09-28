5c17b352ece602320a203d68ee070d2980509a00
ِAbdelrahman Mahmoud <abdelrahmannali14@gmail.com>
2025-09-28 17:32:11 +0300

    enhanced projects section mobile

--- components/projects.vue
+++ components/projects.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
     import { useProjects } from '~/composables/useProjects';
+    import { useDisplay } from 'vuetify';
     const projects= useProjects()
+    const {smAndUp} = useDisplay()
+
 </script>
 
 
@@ -8,7 +11,8 @@
 <section id="#projects" class="py-8">
         <v-container>
           <h2 class="section-title mb-8">Projects</h2>
-          <v-row align="stretch">
+          <template v-if="smAndUp">
+            <v-row align="stretch">
             
             <v-col cols="12" md="4" class="d-flex" v-for="proj in projects" :key="proj.id">
 
@@ -47,7 +51,70 @@
                 
             </v-col>
 
-          </v-row>
+            </v-row>
+          </template>
+
+          <template v-else>
+            <v-carousel
+          show-arrows="hover"
+          hide-delimiters
+          height="420"
+        >
+          <v-carousel-item
+            v-for="proj in projects"
+            :key="proj.id"
+            
+          >
+            <v-card  elevation="6" class="project-card d-flex flex-column h-100 w-100">
+              
+                <v-img :src="proj.thumbnail" height="200px" cover />
+              
+              <v-card-title>{{ proj.project_name }}</v-card-title>
+              
+              <v-card-subtitle v-if="proj.end_date">
+                {{ proj.start_date }} - {{ proj.end_date }}
+              </v-card-subtitle>
+
+              <v-card-text>
+                {{ proj.short_description }}
+              </v-card-text>
+
+              <v-card-text class="pt-0">
+                <v-chip
+                  v-for="tech in proj.technologies"
+                  :key="tech"
+                  size="x-small"
+                  class="ma-1 text-caption"
+                  color="brand"
+                  variant="tonal"
+                  label
+                >
+                  {{ tech }}
+                </v-chip>
+              </v-card-text>
+
+              <v-card-actions class="mt-auto justify-end">
+                <v-btn
+                  v-if="proj.link"
+                  color="accent"
+                  variant="tonal"
+                  :href="proj.link"
+                  target="_blank"
+                  rounded="lg"
+                  density="comfortable"
+                >
+                  View
+                </v-btn>
+              </v-card-actions>
+
+            </v-card>
+            
+          </v-carousel-item>
+        </v-carousel>
+
+          </template>
+          
+
         </v-container>
       </section>
 
