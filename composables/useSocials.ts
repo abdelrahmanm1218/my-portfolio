81c9f3a55abe231d0b54de920633058c993c4c78
ِAbdelrahman Mahmoud <abdelrahmannali14@gmail.com>
2025-10-06 13:13:38 +0300

    Added Gmail link

--- composables/useSocials.ts
+++ composables/useSocials.ts
@@ -1,9 +1,11 @@
 
 export function useSocials(){
     const socials = [
+        {title: 'Gmail',  link: 'mailto:abdelrahmannali14@gmail.com', logo: 'mdi-gmail'},
         {title: 'Linkedin',  link:'https://www.linkedin.com/in/abdelrahman-mahmoud-63031522a', logo: 'mdi-linkedin'},
         {title: 'Github',  link: 'https://github.com/abdelrahmanm1218', logo: 'mdi-github'},
         {title: 'Whatsapp',  link: 'https://wa.me/+201551782849', logo: 'mdi-whatsapp'},
+        
 
     ]
 
