1857b898f6b800c8166f138fdeb3f01fc9f942e5
Abdelrahman Mahmoud <abdelrahmannali14@gmail.com>
2026-04-23 13:56:24 +0200

    minor edits

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,50 +1,14 @@
 import { defineNuxtPlugin } from '#app'
 import { createVuetify } from 'vuetify'
-import {
-  VApp,
-  VContainer,
-  VRow,
-  VCol,
-  VCard,
-  VCardText,
-  VCardTitle,
-  VCardSubtitle,
-  VBtn,
-  VIcon,
-  VChip,
-  VChipGroup,
-  VTimeline,
-  VTimelineItem,
-  VAvatar,
-  VFooter,
-  VDivider,
-  VImg
-} from 'vuetify/components'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    components: {
-      VApp,
-      VContainer,
-      VRow,
-      VCol,
-      VCard,
-      VCardText,
-      VCardTitle,
-      VCardSubtitle,
-      VBtn,
-      VIcon,
-      VChip,
-      VChipGroup,
-      VTimeline,
-      VTimelineItem,
-      VAvatar,
-      VFooter,
-      VDivider,
-      VImg
-    },
+    components,
+    directives,
     theme: {
       defaultTheme: 'light',
       themes: {
@@ -53,18 +17,18 @@ export default defineNuxtPlugin((nuxtApp) => {
             // Brand colors
             primary: '#123524',
             brand: '#123524',
-            
+
             // Neutral colors
             background: '#f8f9fa',
             surface: '#ffffff',
             'surface-variant': '#f1f3f4',
-            
+
             // Text colors
             'on-background': '#1a1a1a',
             'on-surface': '#1a1a1a',
             'on-primary': '#ffffff',
             'on-brand': '#ffffff',
-            
+
             // Additional semantic colors
             secondary: '#6c757d',
             accent: '#C9A66B',
